27efa9c292344f7107f64281afe74f964168f1dc
Freddy Fallon <freddyfallon@gmail.com>
2017-05-24 15:23:36 +0100

    Restyle entry page and make text inputs functional

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, TextInput, Alert, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Card, CardSection, Input, Button } from './common'
+
 import { Actions } from 'react-native-router-flux';
 
 class EntryForm extends Component {
@@ -11,94 +13,40 @@ class EntryForm extends Component {
     };
   }
   render() {
-    return (<View style={styles.container}>
-
-      <View style={styles.inputs}>
-        <View style={styles.selfAddress}>
-          <TextInput
-            style={styles.selfAddressText}
-            onChangeText={(text) => this.setState({ text })}
-            value={this.state.text}
+    return (
+      <View style={styles.container}>
+      <Card>
+        <CardSection>
+          <Input
+            label="Your location"
+            placeholder="Where are you?"
           />
-          <Text>
-            Where are you?
-          </Text>
-        </View>
+        </CardSection>
 
+        <CardSection>
+            <Input
+              label="Their location"
+              placeholder="Where is your friend?"
+            />
+        </CardSection>
 
-        <View style={styles.friendAddress}>
-        <TextInput
-          style={styles.friendAddressText}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-        />
-          <Text>
-            Where are they?
-          </Text>
-        </View>
-      </View>
-
-      <View style={styles.button}>
-        <Button
-          title='GO!'
-          color='blue'
+        <CardSection>
+          <Button
           accessibilityLabel='Click this button to find somewhere you and your friend can meet'
           onPress={Actions.results}
-        />
+          >
+            GO
+          </Button>
+        </CardSection>
+      </Card>
       </View>
-    </View>
     );
   }
 
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'stretch',
-  },
-  inputs: {
-    flex: 3,
-    paddingTop: 50
-  },
-  titleTextStyle: {
-    fontSize: 20
-  },
-  selfAddress: {
-    flex: 10,
-    alignItems: 'center'
-  },
-  selfAddressText: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: 'white'
-  },
-  friendAddress: {
-    flex: 4,
-    alignItems: 'center'
-  },
-  friendAddressText: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: 'white'
-  },
-  submitButton: {
-    borderWidth: 2,
-    height: 100,
-    width: 100,
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  button: {
-    flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  buttonTextStyle: {
-    fontSize: 30
+    flex: 1
   }
 });
 
