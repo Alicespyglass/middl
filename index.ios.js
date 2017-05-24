1b439bca00fb2d62dccb6771765ef1b469ac6e7a
kittysquee <kittysquee@gmail.com>
2017-05-24 10:02:18 +0100

    Adds some basic structure and styling

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View, TouchableHighlight, AppRegistry, StyleSheet, TextInput } from 'react-native';
 
-export default class middl extends Component {
+class Middl extends React.Component {
+  constructor() {
+    super();
+    this.state = { text: 'Enter text'
+      //this is where we will define our state
+    };
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+    return (<View style={styles.container}>
+      
+      <View style={styles.header}>
+        <View style={styles.titleWrapper}>
+          <Text>
+            middl
+          </Text>
+        </View>
+        <View style={styles.selfAddress}>
+          <TextInput
+            style={styles.selfAddressText}
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+          />
+          <Text>
+            Where are you?
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <View style={styles.friendAddress}>
+        <TextInput
+          style={styles.friendAddressText}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+          <Text>
+            Where are they?
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.button}>
+        <TouchableHighlight style={styles.submitButton}
+        underlayColor="gray">
+          <Text>
+            GO!
+          </Text>
+        </TouchableHighlight>
+      </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'stretch',
+    backgroundColor: 'pink'
+  },
+  header: {
+    flex: 1
+  },
+  footer: {
+    flex: 1
+  },
+  titleWrapper: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  selfAddress: {
+    flex: 10,
+    alignItems: 'center'
+  },
+  selfAddressText: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  friendAddress: {
+    flex: 4,
+    alignItems: 'center'
+  },
+  friendAddressText: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  submitButton: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    borderColor: 'red'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  button: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
 });
 
-AppRegistry.registerComponent('middl', () => middl);
+AppRegistry.registerComponent('middl', () => Middl);
