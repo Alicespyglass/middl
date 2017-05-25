efe88c34e77bbaf6139774d9599f7bb5761e2eae
kittysquee <kittysquee@gmail.com>
2017-05-25 10:02:14 +0100

    .

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Card, CardSection, Input, Button } from './common'
 
 class ResultsPage extends Component {
   render() {
-    return (<View>
+    return (
+      <View>
+      <Card>
+
+      </Card>
+
       <View>
         <Text>
-          This is the second page!!!
+          Freddys Place
+          24 Some Street
+          London
+          E4 2AB
         </Text>
       </View>
-    </View>);
+      </View>
+    );
   }
 }
 
