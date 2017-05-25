9fbec1d0da01d497e1fb783687b5bc50264ccb08
kittysquee <kittysquee@gmail.com>
2017-05-25 11:35:56 +0100

    Adds Map

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 class ResultsPage extends Component {
   render() {
-    return (<View>
-      <View>
+    return (<View style={styles.container}>
+
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+
+      <View style={styles.textContainer}>
         <Text>
-          This is the second page!!!
+          Go here
         </Text>
       </View>
+
     </View>);
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    left: 25,
+    right: 25,
+    top: 25,
+    bottom: 25,
+    position: 'absolute'
+  },
+  textContainer: {
+    flex: 1
+  },
+  mapContainer: {
+    flex: 1
+  }
+})
 
 export default ResultsPage;
