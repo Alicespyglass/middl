fabe9c375c3061e0bf32dc36d42ed06a13cea387
Freddy Fallon <freddyfallon@gmail.com>
2017-05-25 16:21:33 +0100

    Add ability to chain API and access route

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -4,29 +4,29 @@ import axios from 'axios';
 import { Button } from './common';
 
 class ResultsPage extends Component {
-  state = {}
+  state = { p1Location: null }
 
   componentWillMount() {
     axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p1)
       .then(response => this.setState({ p1Location: response.data }))
-      .then(axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p2)
-        .then(response => this.setState({ p2Location: response.data })));
+        .then(response => this.setState({ p1Id: this.state.p1Location.results["0"].place_id }))
+      .then(response => axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p2)
+        .then(response => this.setState({ p2Location: response.data })))
+        .then(response => this.setState({ p2Id: this.state.p2Location.results["0"].place_id }))
+        .then(response => axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=place_id:' + this.state.p1Id + '&destination=place_id:' + this.state.p2Id + '&key=AIzaSyDWck9QLMxciHSmTpLCjeohqFLksN6qZHU')
+          .then(response => this.setState({ route: response.data })));
   }
 
   render() {
+    console.log(this.state.route)
     return (<View>
       <View>
         <Text>
-          {this.props.p1}
+          {this.state.p1Id}
         </Text>
         <Text>
-          {this.props.p2}
+          {this.state.p2Id}
         </Text>
-        <Button
-        onPress={console.log(this.state.p1Location.results[0].place_id)}
-        >
-          GO
-        </Button>
       </View>
     </View>);
   }
