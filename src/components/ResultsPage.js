2aa87c06a2b6b8cf8ed9422b902f5a617575810d
Freddy Fallon <freddyfallon@gmail.com>
2017-05-25 15:25:43 +0100

    Attempt chaining API calls

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
+import { Button } from './common';
 
 class ResultsPage extends Component {
   state = {}
 
   componentWillMount() {
     axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p1)
-      .then(response => console.log(response));
+      .then(response => this.setState({ p1Location: response.data }))
+      .then(axios.get('https://maps.googleapis.com/maps/api/geocode/json?&address=' + this.props.p2)
+        .then(response => this.setState({ p2Location: response.data })));
   }
 
   render() {
@@ -19,6 +22,11 @@ class ResultsPage extends Component {
         <Text>
           {this.props.p2}
         </Text>
+        <Button
+        onPress={console.log(this.state.p1Location.results[0].place_id)}
+        >
+          GO
+        </Button>
       </View>
     </View>);
   }
