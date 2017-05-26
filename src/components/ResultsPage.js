1c5ec282ebaa6b6aa21f0821411b3005c0334b0a
Colin Turner <colinturner34@gmail.com>
2017-05-26 10:25:25 +0100

    Add Google Maps Webview to ResultsPage

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, WebView } from 'react-native';
 import MapView from 'react-native-maps';
 import axios from 'axios';
 
+// <MapView.Polyline coordinates={this.state.route} strokeColor={'#55C2DD'} strokeWidth={4}/>
 class ResultsPage extends Component {
   state = { p1Location: null }
 
@@ -17,33 +18,26 @@ class ResultsPage extends Component {
           .then(response => this.setState({ route: response.data })));
   }
 
+
+
+
   render() {
+      // console.log(this.state.route.routes.overview_polyline.points);
+
+
     return (<View style={styles.container}>
 
+
       <View style={styles.mapContainer}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}>
-          <MapView.Marker
-            coordinate={{
-              latitude: 37.78825,
-              longitude: -122.4324
-            }}>
-              <View>
-                <View style={styles.marker} />
-              </View>
-            </MapView.Marker>
-
-          </MapView>
+        <WebView
+          source={{uri: 'https://www.google.co.uk/maps/dir/Makers+Academy,+Commercial+Street,+London/Barrio+Shoreditch,+Shoreditch+High+Street,+London/Old+St,+London/@51.5217865,-0.0851688,16z/data=!3m1!4b1!4m20!4m19!1m5!1m1!1s0x48761caf26599a83:0x9b451d586c649129!2m2!1d-0.0732808!2d51.5173403!1m5!1m1!1s0x48761cba7392d025:0xba621056c1feaa8!2m2!1d-0.0781236!2d51.5259171!1m5!1m1!1s0x48761ca8abba80d9:0xd6cf02f1c545d61e!2m2!1d-0.0882806!2d51.5255374!3e2'}}
+          // style={{marginTop: 20}}
+        />
       </View>
 
-      <View style={styles.textContainer}>
-      </View>
+      <Text style={styles.textContainer}>
+        Go to Old Street
+      </Text>
 
     </View>);
   }
@@ -70,10 +64,10 @@ const styles = StyleSheet.create({
     position: 'absolute'
   },
   textContainer: {
-    flex: 1
+    flex: 2
   },
   mapContainer: {
-    flex: 1
+    flex: 6
   }
 })
 
