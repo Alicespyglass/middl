aaea665509e76cf85afc1fbce8466f675ac9ee74
Freddy Fallon <freddyfallon@gmail.com>
2017-05-26 11:00:19 +0100

    Add hardcoded link to Google Maps route

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, WebView } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, StyleSheet } from 'react-native';
+import { Card, CardSection, Button } from './common';
 import axios from 'axios';
+import getDirections from 'react-native-google-maps-directions';
 
 // <MapView.Polyline coordinates={this.state.route} strokeColor={'#55C2DD'} strokeWidth={4}/>
 class ResultsPage extends Component {
@@ -18,27 +19,42 @@ class ResultsPage extends Component {
           .then(response => this.setState({ route: response.data })));
   }
 
+  handleGetDirections() {
+    const data = {
+       source: {
+        latitude: 51.5463,
+        longitude: 0.0573
+      },
+      destination: {
+        latitude: 51.5175,
+        longitude: 0.0733
+      },
+      params: [
+        {
+          key: 'dirflg',
+          value: 'w'
+        }
+      ]
+    };
 
-
+    getDirections(data);
+}
 
   render() {
       // console.log(this.state.route.routes.overview_polyline.points);
 
 
     return (<View style={styles.container}>
-
-
-      <View style={styles.mapContainer}>
-        <WebView
-          source={{uri: 'https://www.google.co.uk/maps/dir/Makers+Academy,+Commercial+Street,+London/Barrio+Shoreditch,+Shoreditch+High+Street,+London/Old+St,+London/@51.5217865,-0.0851688,16z/data=!3m1!4b1!4m20!4m19!1m5!1m1!1s0x48761caf26599a83:0x9b451d586c649129!2m2!1d-0.0732808!2d51.5173403!1m5!1m1!1s0x48761cba7392d025:0xba621056c1feaa8!2m2!1d-0.0781236!2d51.5259171!1m5!1m1!1s0x48761ca8abba80d9:0xd6cf02f1c545d61e!2m2!1d-0.0882806!2d51.5255374!3e2'}}
-          // style={{marginTop: 20}}
-        />
-      </View>
-
-      <Text style={styles.textContainer}>
-        Go to Old Street
-      </Text>
-
+      <Card>
+        <CardSection>
+          <Button
+          accessibilityLabel='Click this button to find somewhere you and your friend can meet'
+          onPress={this.handleGetDirections}
+          >
+            GO
+          </Button>
+        </CardSection>
+      </Card>
     </View>);
   }
 }
