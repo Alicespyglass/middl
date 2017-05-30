6501a735d8792fcedbae950e2e72559843ea4ab9
alicespyglass <alicespyglass@gmail.com>
2017-05-30 23:40:21 +0100

    styling - added ratings as stars and different alignments for results cards

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -13,7 +13,7 @@ const styles = {
   containerStyle: {
     borderBottomWidth: 0,
     padding: 5,
-    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
     justifyContent: 'flex-start',
     flexDirection: 'row',
     borderColor: '#ddd',
